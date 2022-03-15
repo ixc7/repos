d3ea0a9b270bff2cbaa4eedc327e9cc88ccbec2f
ixc7 <isaacxpreston7@gmail.com>
2022-03-15 09:26:38 -0400

    handle empty description, empty results

--- search.js
+++ search.js
@@ -1,57 +1,60 @@
 #!/usr/bin/env node
 
-import https from 'https'
+import { request } from 'https'
 
 const toURL = q => {
   const API = new URL('/search/repositories', 'https://api.github.com')
   const params = {
-      q,
-      page: 1,
-      per_page: 25,
-      sort: 'stars',
-      order: 'desc',
-      accept: 'application/vnd.github.v3+json',
-    }
+    q,
+    page: 1,
+    per_page: 25,
+    sort: 'stars',
+    order: 'desc',
+    accept: 'application/vnd.github.v3+json'
+  }
   for (const i of Object.keys(params)) {
     API.searchParams.set(i, params[i])
   }
   return API.href
 }
 
-export const search = q => new Promise((resolve, reject) => {
-  const req = https.request(toURL(q), {
-    headers: {
-      'User-Agent': ''
-    }
-  })
-
-  req.on('response', res => {
-    let acc = ''
-    res.on('data', chunk => (acc += chunk.toString('utf8')))
-    res.on('end', () => {
-      let retItems = JSON.parse(acc).items
-      resolve(retItems
-        .map(({
-          html_url,
-          description,
-          default_branch
-        }) => {
-          const max = process.stdout.columns - 15
-          const str = description.slice(0, max).trim()
-          const arr = str.split(' ')
-          const trunc = arr.slice(0, arr.length - 1).join(' ')
-
-          return {
-            REPO: html_url,
-            MAIN: default_branch,
-            INFO: str.length === max ? `${trunc}...` : str
-          }
-        })
-      )
+export const search = (q, opts = {}) =>
+  new Promise((resolve, reject) => {
+    const req = request(toURL(q), {
+      headers: {
+        'User-Agent': ''
+      }
     })
+
+    req.on('response', res => {
+      let acc = ''
+      res.on('data', chunk => (acc += chunk.toString('utf8')))
+      res.on('end', () => {
+        const retItems = JSON.parse(acc).items
+        if (!retItems.length) {
+          console.log('nothing found')
+          process.exit(0)
+        }
+
+        resolve(
+          retItems.map(({ name, html_url, description, default_branch }) => {
+            const max = process.stdout.columns - 24 // '  description: ' (+ padding)
+            const str = description ? description.slice(0, max).trim() : ''
+            const arr = str.split(' ')
+            const trunc = arr.slice(0, arr.length - 1).join(' ')
+
+            return {
+              name,
+              description: str.length === max ? `${trunc}...` : str,
+              repository: html_url,
+              branch: default_branch
+            }
+          })
+        )
+      })
+    })
+    req.on('error', e => reject(e))
+    req.end()
   })
-  req.on('error', e => reject(e))
-  req.end()
-})
 
 export default search
